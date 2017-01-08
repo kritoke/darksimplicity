f1fe5d299d75a163d31c9f2b8942dfd50dd6a545
Matthew Rhone <matthew.rhone@outlook.com>
2017-01-08 12:24:58 -0600

    Start inital schema html adaption, improve gulp workflow, incorporate rucksack.

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -1,19 +1,26 @@
 // Gulp.js configuration
 var gulp = require('gulp'),
+    lost = require('lost'),
+    cssnano = require('cssnano'),
+    autoprefixer = require('autoprefixer')({}),
     watch = require('gulp-watch'),
+    rucksack = require('rucksack-css'),
+    simplevars = require('postcss-simple-vars'),
+    fontmagician = require('postcss-font-magician')({protocol: 'https:'}),
     postcss = require('gulp-postcss');
 
 // apply PostCSS plugins
 gulp.task('css', function() {
   return gulp.src('src/style.min.css')
     .pipe(postcss([
-      require('autoprefixer')({}),
-      require('cssnano'),
-      require('lost'),
-      require('postcss-font-magician')({protocol: 'https:'}),
-      require('postcss-simple-vars')
+      autoprefixer,
+      //cssnano,
+      lost,
+      rucksack,
+      fontmagician,
+      simplevars
     ]))
-    .pipe(gulp.dest('dest'));
+    .pipe(gulp.dest('../static/css'));
 });
 
 gulp.task('default', ['css', 'watch']);
